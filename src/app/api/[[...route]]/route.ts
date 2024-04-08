b6e6dc5df62a0cbabaa96fc61a1122a25521bd5f
Marvin Kaunda <marvinkr@icloud.com>
2024-04-08 23:33:27 +0200

    feat: add hono

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,24 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello World!",
+  });
+});
+
+app.get("/hello/:name", (c) => {
+  return c.json({
+    message: `Hello ${c.req.param("name")}!`,
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
